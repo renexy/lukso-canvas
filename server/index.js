4b81762ff0e82310a353b0b564060755f384cf71
rene.vucko <rene.vucko@comtradegaming.com>
2025-05-04 16:08:11 +0200

    done

--- server/index.js
+++ server/index.js
@@ -100,6 +100,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
+server.listen(process.env.PORT || 5000, () => {
   console.log("Server is running on port 5000");
 });
